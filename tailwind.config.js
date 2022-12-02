07b4a9d93d8f459dadf63b40d69df06f40dc7ea4
PragadheshG <pragadhesh.gopalakrishnan@gds.ey.com>
2022-12-02 19:11:03 +0530

    Updated home page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        dancingscript: ["dancingscript"]
+        dancingscript: ["dancingscript"],
+        noto: ["noto"]
       }
     },
   },
